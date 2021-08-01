b89eb4f709ec348cc314cc3551809c88b846a3e9
Mahesh Biradar <bmahesh66@gmail.com>
2021-08-01 23:27:17 +0530

    Auth Guards

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -6,11 +6,14 @@ import { ArtStarterComponent } from "../art-gallery/art-starter/art-starter.comp
 import { EditArtComponent } from "../art-gallery/edit-art/edit-art.component";
 import { ArtShoppingCartComponent } from "../art-shopping-cart/art-shopping-cart.component";
 import { AuthComponent } from "../auth/auth.component";
+import { AuthGuard } from "../guards/auth.guard";
 import { ArtResolverService } from "../services/art-resolver.service";
 
 const appRoutes: Routes = [
     {path: '', redirectTo:'/arts', pathMatch: 'full'},
-    {path: 'arts', component: ArtGalleryComponent, children: [
+    {path: 'arts', component: ArtGalleryComponent, 
+    canActivate: [AuthGuard],
+    children: [
         { path:'', component: ArtStarterComponent },
         { path:'new', component: EditArtComponent },
         { path:':id', component: ArtDetailComponent, resolve: [ArtResolverService] },
